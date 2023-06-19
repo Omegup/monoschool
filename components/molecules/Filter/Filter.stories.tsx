74281d256dc2b825bd8133bb28710e1c2bb0f0a8
cherif <cherif@omegup.tn>
2023-06-19 09:15:48 +0100

    TEST

---
+++ components/molecules/Filter/Filter.stories.tsx
@@ -0,0 +1,8 @@
+const meta = {
+  title: 'Molecules/Filter)',
+  component: "Your Component",
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} satisfies Meta<typeof  "Filter">;
