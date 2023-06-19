33d67401a3163888313b5a8aaa684440a652d752
cherif <cherif@omegup.tn>
2023-06-19 10:30:51 +0100

    test

--- components/molecules/Filter/Filter.stories.tsx
+++
@@ -1,8 +0,0 @@
-const meta = {
-  title: 'Molecules/Filter)',
-  component: "Your Component",
-  tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} satisfies Meta<typeof  "Filter">;
--- components/molecules/FilterElements/Filter.tsx
+++ components/molecules/FilterElements/Filter.tsx
@@ -1,6 +1,7 @@
 import { colors } from '@omegup-school/ui-atoms/colors';
 import { createUseStyles } from 'react-jss';
 import { styles } from '../common/styles';
+import { useState } from 'react';
 
 const useStyles = createUseStyles({
   filter: {
@@ -10,9 +11,14 @@ const useStyles = createUseStyles({
   },
 });
 
-export type Filter = { text: string };
-
-export const Filter = ({ text }: Filter) => {
+export type Filter = {label:string };
+interface CheckboxProps {
+  label: string;
+}
+export const Filter = ({ label }: Filter) => {
   const classes = useStyles();
-  return <div>filter</div>;
+  
+  return <div>  
+     <label htmlFor={label}>{label}</label>
+  </div>;
 };
