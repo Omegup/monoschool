10fa21cc03f418e19c0d64c509e57b38bcd1fb36
alaBouziri <bouzirialaeddine@gmail.com>
2023-06-21 14:34:42 +0100

    fix tag colors & code optimization

--- components/molecules/Tag/Tag.tsx
+++ components/molecules/Tag/Tag.tsx
@@ -5,27 +5,14 @@ export const Tag = ({ type }: { type: TagType }) => {
 
   const getColor = (type: TagType): { primary: string, backgroundColor: string } => {
 
-    // const map = { success: 'success', warning: 'warning', error: 'danger', active: 'secondary', info: 'light', inactive: 'grey' };
-    // const palette = colors [map[type]];
-    // return { primary: colors.light[400], backgroundColor: colors.light[100] }
-
-
-    switch (type) {
-      case 'success':
-        return { primary: colors.success[600], backgroundColor: colors.success[100] }
-      case 'warning':
-        return { primary: colors.warning[600], backgroundColor: colors.warning[100] }
-      case 'error':
-        return { primary: colors.danger[600], backgroundColor: colors.danger[100] }
-      case 'active':
-        return { primary: colors.secondary[600], backgroundColor: colors.secondary[100] }
-      case 'info':
-        return { primary: colors.blue[600], backgroundColor: colors.light[200] }
-      case 'inactive':
-        return { primary: colors.grey[600], backgroundColor: colors.grey[100] }
-      default:
-        return { primary: colors.success[600], backgroundColor: colors.success[100] }
+    if (type === 'info') {
+    return { primary: colors.blue[600], backgroundColor: colors.light[200] }
+      
     }
+    const map = { success: 'success', warning: 'warning', error: 'danger', active: 'secondary', inactive: 'grey' } as const;
+    const palette = colors [map[type]];
+    return { primary: palette[600], backgroundColor: palette[100] }
+
   }
 
   const styles = useStyles({ color: getColor(type) });
