8ebe038d05ebfdc44d0db07d46f0c1e0aaafda8c
cherif <cherif@omegup.tn>
2023-06-23 17:52:50 +0100

    fix chips

--- components/molecules/Chips/Chips.styles.ts
+++ components/molecules/Chips/Chips.styles.ts
@@ -17,85 +17,108 @@ const chipsSizes = Object.fromEntries<'', Record<'', Sizes>>(
     },
   ])
 );
-console.log(styles);
 
 export const useSelectStyles = createUseStyles({
   solid: {},
   border: {},
-  flat: {},
+  flat: { },
   text: {},
-  large: {},
-  medium: {},
-  small: {},
-  disabled: {},
-  enabled: {},
-  input: {
-    clip: 'rect(0 0 0 0)',
-    position: 'absolute',
-  },
-  label: {
-    display: 'inline-flex',
-
-    '&$disabled > $container': {
-    opacity:"0.4000000059604645",
-    },
-   
-    '&$large > $container': {
+  large: {
+    '& > $container': {
       padding: spacing.s5,
       gap: 7,
-      borderRadius: "14px",
+      borderRadius: '14px',
       border: [borders.b3, 'solid'],
     },
-    '&$medium > $container': {
+  },
+  medium: {
+    '& > $container': {
       padding: spacing.s4,
       gap: spacing.s3,
       borderRadius: 11.2,
       border: [borders.b2, 'solid'],
     },
-    '&$small > $container': {
+  },
+  small: {
+    '& > $container': {
       padding: spacing.s3,
       gap: 5,
       borderRadius: 8.4,
       border: [borders.b1, 'solid'],
     },
-
-    
-    '& > $input:not(:checked) + $container': {
-      borderColor: colors.background.outline,
-      '&:active ': {
-        backgroundColor: colors.background.overlay,
-      },
+  },
+  disabled: {
+    '&$disabled> $container': {
+      opacity: '0.4000000059604645',
     },
-    '& > $input:checked + $container': {
-      color: colors.blue[500],
-      borderColor: colors.blue[500],
-      '&:active ': {
-        backgroundColor: colors.light[400],
-      },
-    },
-    '&:focus': {
-      outline: 'none',
-    },
+  },
+  enabled: {
     '&:focus-within:not(:focus) > $input': {
       '& + $container': {
         outlineWidth: borders.b2,
         outlineColor: colors.blue[600],
         outlineStyle: 'solid',
       },
+     
       '&:not(:checked) + $container': {
         outlineColor: colors.blue[600],
       },
     },
-    '&:hover > $input:checked + $container': {
-      backgroundColor: colors.light[200],
-    },
-    '&:hover > $input:not(:checked) + $container': {
-      backgroundColor: colors.background.secondary,
-    },
-    
-    '&$flat > $input:not(:checked) + $container': {
-      borderColor:"transparent"
+      
+    '& > $input:not(:checked) + $container': {
+      borderColor: colors.background.outline,
+      '&:hover': {
+       backgroundColor: colors.background.secondary, 
       },
+      '&:active': {
+        backgroundColor: colors.background.overlay,
+      },
+    },
+    '& > $input:checked + $container': {
+      color: colors.blue[500],
+      borderColor: colors.blue[500],
+      '&:hover ': {
+        backgroundColor: colors.light[200],
+      },
+      '&:active ': {
+        backgroundColor: colors.light[400],
+      },
+    },
+    '&$flat ': {
+      '& > $input + $container': {
+        borderColor:"transparent !important",
+      },
+      '& > $input:not(:checked) + $container': {
+        backgroundColor: colors.background.secondary,
+        '&:hover': {
+         backgroundColor:colors.background.overlay, 
+        },
+        '&:active': {
+          backgroundColor: colors.background.outline,
+        },
+      },
+      '& > $input:checked + $container': {
+        color: colors.blue[500],
+        backgroundColor: colors.light[100],
+        '&:hover ': {
+          backgroundColor: colors.light[200],
+        },
+        '&:active ': {
+          backgroundColor: colors.light[500],
+        },
+      },
+    },
+   
+  },
+  input: {
+    clip: 'rect(0 0 0 0)',
+    position: 'absolute',
+  },
+  label: {
+    display: 'inline-flex',
+    '&:focus': {
+      outline: 'none',
+    }, 
   },
   container: {
     display: 'flex',
