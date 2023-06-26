116f4cd796108ec60db1b8c0a2b2cf4b2522539a
Adam Abichou <abichouadam48@gmail.com>
2023-06-26 15:42:08 +0100

    test7

--- app/browser/src/test.js
+++ app/browser/src/test.js
@@ -1,18 +1,97 @@
-large: {
-  '& $container': {
-    width: widths.checkbox.large,
-    height: widths.checkbox.large,
-  },
-},
-medium: {
-  '& $container': {
-    width: widths.checkbox.medium,
-    height: widths.checkbox.medium,
-  },
-},
-small: {
-  '& $container': {
-    width: widths.checkbox.small,
-    height: widths.checkbox.small,
-  },
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
 }
+
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
+
+
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
+
+
+
+
