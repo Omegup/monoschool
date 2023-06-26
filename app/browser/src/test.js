8bfb528b5ca4532245e21295ea8ac7df44150b73
Adam Abichou <abichouadam48@gmail.com>
2023-06-26 14:55:34 +0100

    test1

--- app/browser/src/test.js
+++ app/browser/src/test.js
@@ -1,20 +1,32 @@
-large: {
-  '& $container': {
-    width: widths.checkbox.large,
-    height: widths.checkbox.large,
-  },
-},
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
 
-medium: {
-  '& $container': {
-    width: widths.checkbox.medium,
-    height: widths.checkbox.medium,
-  },
-},
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
 
-small: {
-  '& $container': {
-    width: widths.checkbox.small,
-    height: widths.checkbox.small,
-  },
-}, 
+
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
