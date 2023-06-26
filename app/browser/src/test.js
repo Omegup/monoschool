cd385b9fe19dbe150fd50c185ecf0352d38dd2b1
Adam Abichou <abichouadam48@gmail.com>
2023-06-26 15:24:03 +0100

    test5

--- app/browser/src/test.js
+++ app/browser/src/test.js
@@ -14,6 +14,67 @@ if (!doc.exists) {
   }
 }
 
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
+const docRef = firestore.collection("accounts").doc(id);
+const doc = await docRef.get();
+
+if (!doc.exists) {
+  res.status(404).send("Account not found");
+} else {
+  const monsters = doc.data().monsters || {};
+  if (!monsters[mid]) {
+    res.status(404).send("monster not found");
+  } else {
+    delete monsters[mid];
+    await docRef.update({monsters});
+    res.send("Monster deleted successfully");
+  }
+}
+
 
 const docRef = firestore.collection("accounts").doc(id);
 const doc = await docRef.get();
@@ -22,7 +83,7 @@ if (!doc.exists) {
   res.status(404).send("Account not found");
 } else {
   const monsters = doc.data().monsters || {};
-  if (!monsters[mida]) {
+  if (!monsters[mid]) {
     res.status(404).send("monster not found");
   } else {
     delete monsters[mid];
@@ -30,4 +91,7 @@ if (!doc.exists) {
     res.send("Monster deleted successfully");
   }
 }
+
+
+
 
