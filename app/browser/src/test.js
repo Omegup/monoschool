83121f9506933bc61131354d7df7bb72dcb6bec3
Adam Abichou <abichouadam48@gmail.com>
2023-06-26 14:35:57 +0100

    testing-sonarcloud

---
+++ app/browser/src/test.js
@@ -0,0 +1,20 @@
+large: {
+  '& $container': {
+    width: widths.checkbox.large,
+    height: widths.checkbox.large,
+  },
+},
+
+medium: {
+  '& $container': {
+    width: widths.checkbox.medium,
+    height: widths.checkbox.medium,
+  },
+},
+
+small: {
+  '& $container': {
+    width: widths.checkbox.small,
+    height: widths.checkbox.small,
+  },
+}, 
