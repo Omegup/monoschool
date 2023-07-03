1684332d926be379b0874e53a3978c36c2e3c88b
souhaib02 <med.souhaib@gmail.com>
2023-07-03 17:15:46 +0100

    tooltip

--- components/molecules/Tooltip/Tooltip.tsx
+++ components/molecules/Tooltip/Tooltip.tsx
@@ -3,11 +3,12 @@ import { TooltipProps } from './Tooltip.types'
 
 export const Tooltip = ({ size, style, position, label }: TooltipProps) => {
   const classes = useStyles()
-  console.log(classes)
+
   return (
-    <span className={`${classes.container} ${classes[`flexDirection${position}`]} ${classes[style]} ${classes[size]}`}>
+    <span className={`${classes[style]} ${classes[size]}`}>
+      <span className={`${classes.container} ${classes[position]}`}>
         <span className={classes.label}>{label}</span>
-        <span className={classes[`triangle${position}`]}>
+        <span className={`${classes.triangle}`}></span>
       </span>
     </span>
   )
