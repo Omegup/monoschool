cde53a13dfe14c6fe19671d76e5c8dbb97e48e57
cherif <cherif@omegup.tn>
2023-07-03 14:45:34 +0100

    fix

--- storybook/molecules/SideBar.stories.tsx
+++ storybook/molecules/SideBar.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { SideBar, SearchBar } from '@omegup-school/ui-molecules';
+import { SideBar } from '@omegup-school/ui-molecules';
 import { SearchStatus} from '@omegup-school/ui-assets';
 import { Logo } from '@omegup-school/ui-assets';
 const meta = {
@@ -45,8 +45,8 @@ export const Primary: Story = {
       size: 'medium',
       title: 'Omega School',
       Logo: <Logo width={"100%"} />,
-      Search:<SearchBar value={''} onChange={()=>{} } onBlur={()=>{} } size={'large'} style={'solid'} displayClose={false} label={'Rechercher'} 
-      Icon={SearchStatus}/>,
+      Search:/*/<SearchBar value={''} onChange={()=>{} } onBlur={()=>{} } size={'large'} style={'solid'} displayClose={false} label={'Rechercher'} 
+      Icon={SearchStatus}/>/*/"SearchBar",
       SearchIcon:SearchStatus ,
     },
     button:{
