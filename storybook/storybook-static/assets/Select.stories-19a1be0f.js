b9cb2bed96d3c4774610938668b706c7916397ab
Wissem <wissem.slim@omegup.tn>
2023-07-04 09:05:32 +0100

    duild

---
+++ storybook/storybook-static/assets/Select.stories-19a1be0f.js
@@ -0,0 +1,12 @@
+import{S as r}from"./TextWithIcon-5e2b8c74.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./assertThisInitialized-731f44fd.js";const m={title:"Atom/Select",component:r,tags:["autodocs"],argTypes:{style:{control:"select",options:["solid","border","text","flat"]},size:{control:"select",options:["large","medium","small"]}}},e={args:{label:"Select",size:"medium",style:"solid",disabled:!1,value:!0,onChange:()=>{},onBlur:()=>{}}};var t,o,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: 'Select',
+    size: 'medium',
+    style: 'solid',
+    disabled: false,
+    value: true,
+    onChange: () => {},
+    onBlur: () => {}
+  }
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const c=["Primary"];export{e as Primary,c as __namedExportsOrder,m as default};
+//# sourceMappingURL=Select.stories-19a1be0f.js.map
