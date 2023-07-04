65f3a4b6e0cf97f9013715719ee05e45c4d50f6f
bourguiba <bourguiba@omegup.tn>
2023-07-04 12:14:02 +0100

    wip

---
+++ components/assets/icons/ClipboardTick.tsx
@@ -0,0 +1,27 @@
+import { widths } from '@omegup-school/ui-atoms/sizes';
+
+export const ClipboardTick = ({
+  width = widths.icon.svg,
+  height = width,
+  color = 'currentColor',
+}: {
+  width?: string;
+  height?: string;
+  color?: string;
+}) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+   <g id="clipboard-tick">
+<path id="Vector" d="M9.31006 14.7002L10.8101 16.2002L14.8101 12.2002" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path id="Vector_2" d="M10 6H14C16 6 16 5 16 4C16 2 15 2 14 2H10C9 2 8 2 8 4C8 6 9 6 10 6Z" stroke={color} strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+<path id="Vector_3" d="M16 4.02002C19.33 4.20002 21 5.43002 21 10V16C21 20 20 22 15 22H9C4 22 3 20 3 16V10C3 5.44002 4.67 4.20002 8 4.02002" stroke={color} strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+</g>
+    </svg>
+  );
+};
