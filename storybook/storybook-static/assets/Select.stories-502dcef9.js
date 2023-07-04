426046cbb4efc29fd9b20097720209e047914c9b
cherif <cherif@omegup.tn>
2023-07-04 09:42:07 +0100

    build

---
+++ storybook/storybook-static/assets/Select.stories-502dcef9.js
@@ -0,0 +1,11 @@
+import{S as o}from"./Chips-de9bda84.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./assertThisInitialized-731f44fd.js";const m={title:"Atom/Select",component:o,tags:["autodocs"],argTypes:{style:{control:"select",options:["solid","border","text","flat"]},size:{control:"select",options:["large","medium","small"]}}},e={args:{label:"Select",size:"medium",style:"solid",disabled:!1,value:!1,onChange:()=>{}}};var s,t,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    label: 'Select',
+    size: 'medium',
+    style: 'solid',
+    disabled: false,
+    value: false,
+    onChange: () => {}
+  }
+}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const c=["Primary"];export{e as Primary,c as __namedExportsOrder,m as default};
+//# sourceMappingURL=Select.stories-502dcef9.js.map
