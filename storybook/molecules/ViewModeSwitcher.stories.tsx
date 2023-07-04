f0c7a17d26e1db9070ff6a51548bfea62d6d73f6
bourguiba <bourguiba@omegup.tn>
2023-07-04 15:41:48 +0100

    wip

--- storybook/molecules/ViewModeSwitcher.stories.tsx
+++ storybook/molecules/ViewModeSwitcher.stories.tsx
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { GridElement, TextAline } from '../../components/assets'
-
 import { ViewModeSwitcher } from '@omegup-school/ui-molecules';
-const elementsViewModeRight = { id: "1", icon: <GridElement /> }
 
+const elementsViewModeRight = { id: "1", icon: <GridElement /> }
 const elementsViewModeLeft = { id: "2", icon: <TextAline />}
+
 const meta = {
   title: 'Atom/ViewModeSwitcher',
   component: ViewModeSwitcher,
