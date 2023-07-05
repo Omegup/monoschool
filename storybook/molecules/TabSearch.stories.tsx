6844ab0e5ca1dbc1130d30df43e79176a7da3e7b
ahmed <ahmed.lazher@omegup.tn>
2023-07-05 16:24:52 +0100

    edit story

--- storybook/molecules/TabSearch.stories.tsx
+++ storybook/molecules/TabSearch.stories.tsx
@@ -15,14 +15,11 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     selectedIndex: 0,
-    setSelectedIndex(index: number) {
-      console.log(index);
-    },
     tabProps: [
       { label: 'Tous', badge: 20 },
       { label: 'Apprenant', badge: 12 },
       { label: 'Parents', badge: 3 },
-      { label: 'Professeur', badge: 1 },
+      { label: 'Professeur' },
     ],
   },
 };
