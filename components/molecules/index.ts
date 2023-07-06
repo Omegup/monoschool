2f85500ee5c328030eae3cf63135ad16c71e2b21
bourguiba <bourguiba@omegup.tn>
2023-07-06 12:54:38 +0100

    wip

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,7 +1,4 @@
-export * from './Button';
-export * from './Link';
-export * from './Panel';
-export * from './Select';
+
 export * from './SignIn';
 export * from './SubMenu';
 export * from './ChildMenu';
