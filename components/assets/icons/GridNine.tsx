a3284129bad0d0418626af25eadc2b81162ada9e
alaBouziri <bouzirialaeddine@gmail.com>
2023-07-12 14:29:49 +0100

    fix styling icons and spacing

--- components/assets/icons/GridNine.tsx
+++ components/assets/icons/GridNine.tsx
@@ -11,7 +11,7 @@ export const GridNine = ({
     <svg
       width={width}
       height={height}
-      viewBox="0 0 23 22"
+      viewBox={bold ? "0 0 22 20" : "0 0 23 22"}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
--- components/assets/icons/GridSix.tsx
+++ components/assets/icons/GridSix.tsx
@@ -12,7 +12,7 @@ export const GridSix = ({
       width={width}
       height={height}
       fill="none"
-      viewBox="0 0 23 22"
+      viewBox={bold ? "0 0 22 21" : "0 0 23 22"}
       xmlns="http://www.w3.org/2000/svg"
     >
       {bold ? (
