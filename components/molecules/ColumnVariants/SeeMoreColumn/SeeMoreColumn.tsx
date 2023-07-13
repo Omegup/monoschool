7361a109a7e44699f4c92fb457df4ddec9898da9
Farhat Baaroun <69281333+GldzzPro@users.noreply.github.com>
2023-07-13 10:46:45 +0100

    adding tagColumn story

--- components/molecules/ColumnVariants/SeeMoreColumn/SeeMoreColumn.tsx
+++ components/molecules/ColumnVariants/SeeMoreColumn/SeeMoreColumn.tsx
@@ -1,14 +1,10 @@
 import { Container, Link } from "@omegup-school/ui-atoms"
 import { ControlledSeeMoreColumnProps } from "./SeeMoreColumn.types"
 
-//TODO : remove fragments after Container typing  is fixed
-
-
 export const SeeMoreColumn = ({ text, onClick }: ControlledSeeMoreColumnProps) => {
   return (
     <Container onClick={onClick} paddingBlock="s7" paddingInline="s7">
       <Link text={text} />
-      <></>
     </Container>
   )
 }
