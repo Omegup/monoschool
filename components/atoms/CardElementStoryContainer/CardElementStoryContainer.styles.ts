63e5ef81e810cb37c86a2dbc6916befe4bc2d8d3
bourguiba <bourguiba@omegup.tn>
2023-07-13 09:14:12 +0100

    wip

--- components/atoms/CardElementStoryContainer/CardElementStoryContainer.styles.ts
+++ components/atoms/CardElementStoryContainer/CardElementStoryContainer.styles.ts
@@ -1,19 +1,19 @@
 import { createUseStyles } from "react-jss";
-import { ContainerProps } from "./CardElementStoryContainer.types";
+import { CardElementStoryContainerProps } from "./CardElementStoryContainer.types";
 import { spacing } from "@omegup-school/ui-configs/sizes";
 import { Avatar } from "@omegup-school/ui-assets";
 import { colors } from "@omegup-school/ui-configs/colors";
 
-export const useContainerStyles = createUseStyles({
+export const useCardElementStoryContainerStyles = createUseStyles({
   container: {
-    display:  ({ display }: Partial<ContainerProps>) => display || "flex",
-    flexDirection: ({ direction }: Partial<ContainerProps>) => direction,
-    paddingInline: ({ paddingInline }: Partial<ContainerProps>) => paddingInline ? spacing[paddingInline] : 0,
-    paddingBlock: ({ paddingBlock }: Partial<ContainerProps>) => paddingBlock ? spacing[paddingBlock] : 0,
-    gap: ({ gap }: Partial<ContainerProps>) => gap ? spacing[gap] : 0,
-    alignItems: ({ alignItems }: Partial<ContainerProps>) => alignItems || "start",
-    justifyContent: ({ justifyContent }: Partial<ContainerProps>) => justifyContent || "start",
-    width:({ width }: Partial<ContainerProps>) => width || "-webkit-fill-available",
+    display:  ({ display }: Partial<CardElementStoryContainerProps>) => display || "flex",
+    flexDirection: ({ direction }: Partial<CardElementStoryContainerProps>) => direction,
+    paddingInline: ({ paddingInline }: Partial<CardElementStoryContainerProps>) => paddingInline ? spacing[paddingInline] : 0,
+    paddingBlock: ({ paddingBlock }: Partial<CardElementStoryContainerProps>) => paddingBlock ? spacing[paddingBlock] : 0,
+    gap: ({ gap }: Partial<CardElementStoryContainerProps>) => gap ? spacing[gap] : 0,
+    alignItems: ({ alignItems }: Partial<CardElementStoryContainerProps>) => alignItems || "start",
+    justifyContent: ({ justifyContent }: Partial<CardElementStoryContainerProps>) => justifyContent || "start",
+    width:({ width }: Partial<CardElementStoryContainerProps>) => width || "-webkit-fill-available",
     '&:hover': { borderRadius: "60px",
     border: "5px solid var(--blue-blue, #3DA9FC)",
     background: "url(<path-to-image>), lightgray 50% / cover no-repeat",
--- components/molecules/CardElementStory/CardElementStory.types.ts
+++ components/molecules/CardElementStory/CardElementStory.types.ts
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 export interface  CardElementStorytProps {
   text?: string;
-  avatar?:ReactNode
+  avatarUrl?:string
  }
  export interface  ControlledCardElementStorytProps extends CardElementStorytProps {
   onclick: (value: boolean) => void;
