501208564c49af838630e282bba05df9471caadb
Farhat Baaroun <69281333+GldzzPro@users.noreply.github.com>
2023-07-13 11:02:18 +0100

    AvatarColumn is done

--- components/molecules/ColumnVariants/AvatarColumn/AvatarColumn.tsx
+++ components/molecules/ColumnVariants/AvatarColumn/AvatarColumn.tsx
@@ -6,7 +6,7 @@ export const AvatarColumn = forwardRef((
   { avatar, text }: AvatarColumnProps,
   ref?: React.Ref<HTMLDivElement>
 ) => {
-  return <Container ref={ref} gap='s5' paddingBlock="s5" paddingInline="s5" >
+  return <Container ref={ref} gap='s5' paddingBlock="s5" paddingInline="s5" alignItems="center" >
     {avatar}
     <Text ref={ref} text={text} variant="paragraph_xSmall_regular" />
   </Container>
