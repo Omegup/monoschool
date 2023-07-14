81a4d2367489c0b8b54788111cb4da347b42026f
souhaib02 <med.souhaib@gmail.com>
2023-07-14 09:01:30 +0100

    TooltipStory

---
+++ storybook/cells/MenuStory.tsx
@@ -0,0 +1,33 @@
+
+import { Popover } from '@omegup-school/ui-cells'
+
+export const TooltipStory = () => {
+
+  return (<>
+    <Popover preferredPosition="bottom-center" backdrop>
+      <Popover.Trigger showOn='click' jump>
+        <input placeholder="Search" />
+      </Popover.Trigger>
+      <Popover.Content>
+        <ul>
+          <li>
+            <Popover preferredPosition="right-center"  backdrop>
+              <Popover.Trigger showOn='click'>
+                <a href="#">Students</a>
+              </Popover.Trigger>
+              <Popover.Content>
+                <ul>
+                  <li>Sub 11111111111111111</li>
+                  <li>Sub 222222222222222222</li>
+                </ul>
+              </Popover.Content>
+            </Popover>
+
+          </li>
+          <li>Student B</li>
+          <li>Student C</li>
+        </ul>
+      </Popover.Content>
+    </Popover>
+  </>)
+}
