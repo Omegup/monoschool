2d6fb791be5de606e375cc3cff8ddb6a5b10d747
cherif <cherif@omegup.tn>
2023-07-14 16:03:29 +0100

    fixing

--- components/atoms/ItemCardNumberDashboard/ItemCardNumberDashboard.styles.ts
+++ components/atoms/ItemCardNumberDashboard/ItemCardNumberDashboard.styles.ts
@@ -1,39 +1,44 @@
 import { colors } from '@omegup-school/ui-configs/colors';
+import { shadowSmall } from '@omegup-school/ui-configs/effects/shadow';
 import { borders, spacing, widths } from '@omegup-school/ui-configs/sizes';
 import { createUseStyles } from 'react-jss';
 
 export const useCardNumberDashboardStyles = createUseStyles({
   container: {
     display: 'flex',
-    paddingBlock: '21px',
+    paddingBlockStart: spacing.s9,
+    paddingBlockEnd: spacing.s3,
     justifyContent: 'center',
     alignItems: 'center',
     gap: spacing.s3,
     borderRadius: borders.r5,
-    position: 'relative',
     userSelect: 'none',
     cursor: 'pointer',
     width: widths.itemCardNumberDashboard.default,
+    backgroundColor: colors.headline[100],
+    transition: 'border-bottom-color 800ms ease',
+    borderBottom: [borders.b11,"solid","transparent"],
+    boxShadow:shadowSmall,
   },
   blue: {
     backgroundColor: colors.blue[500],
     color: colors.blue[100],
-    '&:hover > $bottomLine': {
-      backgroundColor: colors.headline[500],
+    '&:hover': {
+      borderBottomColor: colors.headline[500],
     },
   },
   headLine: {
     backgroundColor: colors.headline[500],
     color: colors.headline[100],
-    '&:hover > $bottomLine': {
-      backgroundColor: colors.headline[100],
+    '&:hover': {
+      borderBottomColor: colors.headline[100],
     },
   },
   light: {
     backgroundColor: colors.light[500],
     color: colors.blue[500],
-    '&:hover > $bottomLine': {
-      backgroundColor: colors.blue[500],
+    '&:hover ': {
+      borderBottomColor: colors.blue[500],
     },
   },
   childrenContainer: {
@@ -46,14 +51,5 @@ export const useCardNumberDashboardStyles = createUseStyles({
         height:widths.itemCardNumberDashboard.icon,
     }
   },
-  bottomLine: {
-    position: 'absolute',
-    bottom: '0px',
-    width: '100%',
-    height: widths.itemCardNumberDashboard.heightBotomBorder,
-    borderBottomRightRadius: borders.r5,
-    borderBottomLeftRadius: borders.r5,
-    backgroundColor: 'transparent',
-    transition: 'background-color 800ms ease',
-  },
+  
 });
