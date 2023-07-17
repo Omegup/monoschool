cb7137404c6bb440e2a5f16640950d5e3b47c6b9
adamab48 <abichouadam48@gmail.com>
2023-07-17 10:57:21 +0100

    just checking if rules are still enforced

---
+++ app/browser/src/test.tsx
@@ -0,0 +1,11 @@
+// Potentially falsey strings are not allowed
+let str = '';
+<App>{str && <Foo />}</App>;
+
+// Potentially falsey numbers are not allowed
+let num = 0;
+<App>{num && <Foo />}</App>;
+
+// Includes types that may be a string or number
+let thisOrThat: Record<any, any> | string | number;
+<App>{thisOrThat && <Foo />}</App>;
